b3d550a4682d74f1fb5fd675539e2e96256fd593
andre.teixeira <andre-x8@hotmail.com>
2022-09-07 16:51:26 -0300

    Adicionado conexão com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+ 
+    /**Inicio da configuração da conexão com o banco Postgress
+     * Antes da configuração inicial necessita desse pacote instalado 
+     * npm install --save @nestjs/typeorm pg
+     */
+     imports: [ConfigModule.forRoot() ,TypeOrmModule.forRootAsync({
+    useFactory:()=> ({
+      type:"postgres",
+      host:"localhost",
+      port:5432,
+      username:"postgres",
+      password:"admin",
+      database:"blog",
+      entities:[__dirname+"/**/*.entity{.ts,.js}"],
+      synchronize:true
+    
+    })
+  }),],
   controllers: [AppController],
   providers: [AppService],
+   
 })
 export class AppModule {}
